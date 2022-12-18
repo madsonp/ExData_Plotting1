1d10e915d9a9a8f95bf518bb9f9743d37b596f7e
Madson <madsonlima@gmail.com>
2022-12-18 09:39:26 -0300

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,14 @@
+data <- read.csv("D:\User\Courses\R Programming\Week1//household_power_consumption.txt", sep=";", na.strings="?",stringsAsFactors = FALSE)
+df<- subset(data, data$Date=="1/2/2007"|data$Date=="2/2/2007")
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+df$Datetime <-paste(df$Date, df$Time)
+
+#Plot2
+png("plot2.png", width=480, height=480)
+plot(strptime(df$Datetime, "%d/%m/%Y %H:%M:%S"), df$Global_active_power, type="l", xlab="",ylab="Global Active Power(Kilowatts)" )
+dev.off()
